18d03b9d9868c8a58b9cacb3aa3f3ff98e37abc6
Nodirbek <nbekdev@gmail.com>
2024-08-20 18:19:31 +0500

    fix it

--- src/components/AviaTicket.vue
+++ src/components/AviaTicket.vue
@@ -4,21 +4,14 @@ import IconTwoArrow from "@/components/icons/IconTwoArrow.vue";
 import IconKalendar from "./icons/IconKalendar.vue";
 import IconUser from "./icons/IconUser.vue";
 import IconSearch from "./icons/IconSearch.vue";
-import IconLocs from "./icons/IconLocs.vue";
 
 import TicketCard from "./TicketCard.vue";
 </script>
 <template>
-  <div class="" >
-    <div class="py-4 bg-main">
-      <div class="flex flex-col items-center">
-        <h2 class="text-3xl font-semibold">Aвиабилеты</h2>
-        <p class="text-lg font-medium text-bold-blue">Путешествия</p>
-      </div>
-    </div>
-    <div class="container mx-auto max-xl:px-[15px]  xl:px-0">
+  <div class="">
+    <div class="container mx-auto max-xl:px-[15px] xl:px-0">
       <div
-        class="flex items-center w-full gap-2 my-3 text-sm font-medium text-light-dark "
+        class="flex items-center w-full gap-2 my-3 text-sm font-medium text-light-dark"
       >
         <span>Главная</span>
         <div class="flex items-center gap-2">
@@ -38,7 +31,7 @@ import TicketCard from "./TicketCard.vue";
             class="flex w-full my-3 bg-white rounded lg:h-10 max-lg:flex-wrap border-bor max-lg:bg-transparent max-sm:border-none max-w-max"
           >
             <div
-              class="relative flex flex-wrap max-sm:px-1 max-lg:w-max gap-y-2 max-sm:w-full "
+              class="relative flex flex-wrap max-sm:px-1 max-lg:w-max gap-y-2 max-sm:w-full"
             >
               <input
                 type="text"
@@ -49,7 +42,7 @@ import TicketCard from "./TicketCard.vue";
                 type="text"
                 placeholder="Станция прибытия"
                 class="h-10 pl-5 border outline-none border-bor max-lg:rounded-r-lg max-lg:w-64 max-sm:w-full max-sm:rounded-xl max-sm:px-2 max-lg:border lg:w-70"
-              /> 
+              />
               <span
                 class="absolute flex items-center justify-center w-8 h-8 -ml-4 text-lg bg-white border rounded-full text-bold-blue left-1/2 top-1 border-bor max-sm:top-5 max-sm:mt-2"
               >
@@ -60,7 +53,7 @@ import TicketCard from "./TicketCard.vue";
               class="flex flex-wrap h-10 max-lg:mt-4 max-lg:w-full max-lg:gap-4 max-sm:gap-2 max-sm:mt-2"
             >
               <span
-                class="flex items-center justify-between  gap-2 px-4 max-sm:px-2 max-sm:mx-1 max-sm:rounded-xl h-10 border-x max-lg:bg-white border max-lg:rounded-lg max-lg:w-62 max-sm:w-full lg:w-[180px] border-bor"
+                class="flex items-center justify-between gap-2 px-4 max-sm:px-2 max-sm:mx-1 max-sm:rounded-xl h-10 border-x max-lg:bg-white border max-lg:rounded-lg max-lg:w-62 max-sm:w-full lg:w-[180px] border-bor"
               >
                 <!-- <input
                   type="text"
@@ -85,50 +78,48 @@ import TicketCard from "./TicketCard.vue";
           </div>
           <div class="max-xl:hidden max-lg:flex max-md:hidden">
             <button
-              class="flex h-10 text-white rounded-lg bg-bold-blue w-50 max-lg:w-40 "
+              class="flex h-10 text-white rounded-lg bg-bold-blue w-50 max-lg:w-40"
             >
-              <span class="flex items-center justify-center w-full h-full gap-2 gap-y-2">
+              <span
+                class="flex items-center justify-center w-full h-full gap-2 gap-y-2"
+              >
                 <icon-search />
-                <span>Найти  </span>
+                <span>Найти </span>
               </span>
             </button>
           </div>
         </div>
-        <div class="px-1 sm:hidden max-sm:flex max-sm:w-full max-sm:mt-12 ">
+        <div class="px-1 sm:hidden max-sm:flex max-sm:w-full max-sm:mt-12">
           <!-- max-sm:mt-44 -->
-          <button class="w-full h-[43px] mb-4 text-white rounded-lg bg-bold-blue ">
-            <span class="flex items-center justify-center gap-2 ">
+          <button
+            class="w-full h-[43px] mb-4 text-white rounded-lg bg-bold-blue"
+          >
+            <span class="flex items-center justify-center gap-2">
               <icon-search />
               <span>Найти </span>
             </span>
           </button>
         </div>
         <div
-          class="flex px-2 max-xl:justify-between max-sm:flex-col gap-y-2 max-lg:px-0 "
+          class="flex px-2 max-xl:justify-between max-sm:flex-col gap-y-2 max-lg:px-0"
         >
-          <span
-            class="flex items-center h-10 gap-2 px-4 text-sm bg-white border rounded-lg max-sm:w-full max-lg:justify-center text-light-dark border-bor"
+         
+          <div
+            class="max-xl:flex xl:hidden max-lg:hidden max-md:flex max-sm:hidden"
           >
-            <icon-locs class="text-lg" />
-            <!-- <input
-              type="text"
-              placeholder="Составить сложный маршрут"
-              class="w-48 h-full outline-none"
-            /> -->
-            <span> Составить сложный маршрут </span>
-          </span>
-          <div class="max-xl:flex xl:hidden max-lg:hidden max-md:flex max-sm:hidden">
             <button class="h-full text-white rounded-lg bg-bold-blue w-50">
-              <span class="flex items-center gap-2 px-16 ">
+              <span class="flex items-center gap-2 px-16">
                 <icon-search />
-                <span>Найти  </span>
+                <span>Найти </span>
               </span>
             </button>
           </div>
         </div>
       </div>
     </div>
-    <div class="container flex flex-col max-xl:px-[15px] pb-10 mx-auto mt-4 xl:gap-y-6 max-xl:gap-y-4 xl:px-0">
+    <div
+      class="container flex flex-col max-xl:px-[15px] pb-10 mx-auto mt-4 xl:gap-y-6 max-xl:gap-y-4 xl:px-0"
+    >
       <ticket-card />
       <ticket-card />
       <ticket-card />
